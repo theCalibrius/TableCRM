870efb711106b9793e2d4361b536dc144e1b67a0
Masato Miura <hi@masato.io>
2017-11-01 18:55:01 -0700

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -4,30 +4,39 @@
 -- mysql -u root table_crm < schema.sql
 -- \. schema.sql
 -- mysql -u root < schema.sql
-DROP DATABASE table_crm;
 
+DROP DATABASE IF EXISTS table_crm;
 CREATE DATABASE table_crm;
-SET SQL_MODE="";
 USE table_crm;
 
-DROP TABLE IF EXISTS `leads`;
+DROP TABLE IF EXISTS leads;
+CREATE TABLE leads (
+  id int NOT NULL AUTO_INCREMENT,
+  ownerId int,
+  description varchar(255),
+  firstName varchar(255),
+  lastName varchar(255),
+  suffix varchar(255),
+  title varchar(255),
+  value int,
+  email varchar(255),
+  phoneNumber varchar(255),
+  createdDate datetime NOT NULL DEFAULT NOW(),
+  updatedDate date,
+  PRIMARY KEY (ID)
+);
 
-CREATE TABLE `leads` (
-  `id` INTEGER NOT NULL AUTO_INCREMENT,
-  `ownerId` INTEGER NOT NULL,
-  `description` VARCHAR(255),
-  `firstName` VARCHAR(255),
-  `lastName` VARCHAR(255),
-  `suffix` VARCHAR(255),
-  `title` VARCHAR(255),
-  `value` INTEGER,
-  `email` VARCHAR(255),
-  `phoneNumber` VARCHAR(255),
-  `createdDate` DATETIME NOT NULL DEFAULT NOW(),
-  `updatedDate` DATE ,
-  CONSTRAINT `pk_Leads` PRIMARY KEY (
-    `id`
-  )
-)
-
--- INSERT INTO leads (description, firstName, lastName, suffix, title, value, email, phoneNumber, ownerId) VALUES ('description', 'firstName', 'lastName', 'suffix', 'title', 10000000, 'hi@masato.io', 5623381621, 1);
+INSERT INTO leads (description, firstName, lastName, suffix, title, value, email, phoneNumber, ownerId) VALUES ('description', 'firstName', 'lastName', 'suffix', 'title', 10000000, 'hi@masato.io', 5623381621, 1);
+INSERT INTO leads (description, firstName, lastName, suffix, title, value, email, phoneNumber, ownerId) VALUES ('description', 'firstName', 'lastName', 'suffix', 'title', 10000000, 'hi@masato.io', 5623381621, 1);
+INSERT INTO leads (description, firstName, lastName, suffix, title, value, email, phoneNumber, ownerId) VALUES ('description', 'firstName', 'lastName', 'suffix', 'title', 10000000, 'hi@masato.io', 5623381621, 1);
+INSERT INTO leads (description, firstName, lastName, suffix, title, value, email, phoneNumber, ownerId) VALUES ('description', 'firstName', 'lastName', 'suffix', 'title', 10000000, 'hi@masato.io', 5623381621, 1);
+INSERT INTO leads (description, firstName, lastName, suffix, title, value, email, phoneNumber, ownerId) VALUES ('description', 'firstName', 'lastName', 'suffix', 'title', 10000000, 'hi@masato.io', 5623381621, 1);
+INSERT INTO leads (description, firstName, lastName, suffix, title, value, email, phoneNumber, ownerId) VALUES ('description', 'firstName', 'lastName', 'suffix', 'title', 10000000, 'hi@masato.io', 5623381621, 1);
+INSERT INTO leads (description, firstName, lastName, suffix, title, value, email, phoneNumber, ownerId) VALUES ('description', 'firstName', 'lastName', 'suffix', 'title', 10000000, 'hi@masato.io', 5623381621, 1);
+INSERT INTO leads (description, firstName, lastName, suffix, title, value, email, phoneNumber, ownerId) VALUES ('description', 'firstName', 'lastName', 'suffix', 'title', 10000000, 'hi@masato.io', 5623381621, 1);
+INSERT INTO leads (description, firstName, lastName, suffix, title, value, email, phoneNumber, ownerId) VALUES ('description', 'firstName', 'lastName', 'suffix', 'title', 10000000, 'hi@masato.io', 5623381621, 1);
+INSERT INTO leads (description, firstName, lastName, suffix, title, value, email, phoneNumber, ownerId) VALUES ('description', 'firstName', 'lastName', 'suffix', 'title', 10000000, 'hi@masato.io', 5623381621, 1);
+INSERT INTO leads (description, firstName, lastName, suffix, title, value, email, phoneNumber, ownerId) VALUES ('description', 'firstName', 'lastName', 'suffix', 'title', 10000000, 'hi@masato.io', 5623381621, 1);
+INSERT INTO leads (description, firstName, lastName, suffix, title, value, email, phoneNumber, ownerId) VALUES ('description', 'firstName', 'lastName', 'suffix', 'title', 10000000, 'hi@masato.io', 5623381621, 1);
+INSERT INTO leads (description, firstName, lastName, suffix, title, value, email, phoneNumber, ownerId) VALUES ('description', 'firstName', 'lastName', 'suffix', 'title', 10000000, 'hi@masato.io', 5623381621, 1);
+INSERT INTO leads (description, firstName, lastName, suffix, title, value, email, phoneNumber, ownerId) VALUES ('description', 'firstName', 'lastName', 'suffix', 'title', 10000000, 'hi@masato.io', 5623381621, 1);
