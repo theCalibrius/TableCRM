6f4c7d7b4f8b7b87aa8aaa90c5bc6557c1b5f626
masato <hi@masato.io>
2017-11-03 09:45:50 -0700

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,21 +6,21 @@ import Opportunities from '../container/Opportunities.jsx';
 import People from './People.jsx';
 import Companies from './Companies.jsx';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <div>Header</div>
-        <Switch>
-          <exact path="/dashboard" component={Dashboard} />
-          <Route exact path="/people" component={People} />
-          <Route exact path="/companies" component={Companies} />
-          <Route exact path="/leads" component={Leads} />
-          <Route exact path="/opportunities" component={Opportunities} />
-        </Switch>
-      </div>
-    );
-  }
-}
-
-export default App;
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <div>Header</div>
+//         <Switch>
+//           <Route exact path="/dashboard" component={Dashboard} />
+//           <Route exact path="/people" component={People} />
+//           <Route exact path="/companies" component={Companies} />
+//           <Route exact path="/leads" component={Leads} />
+//           <Route exact path="/opportunities" component={Opportunities} />
+//         </Switch>
+//       </div>
+//     );
+//   }
+// }
+//
+// export default App;
