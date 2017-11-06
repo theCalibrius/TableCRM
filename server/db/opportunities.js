75cafd7c995687fa08e4a04355e4519ab45a9ae6
Joey Li <joeyzaozaoli@gmail.com>
2017-11-05 18:44:31 -0800

    Implement route of POST at /api/opportunities

--- server/db/opportunities.js
+++ server/db/opportunities.js
@@ -6,6 +6,17 @@ const getAllOpportunities = (req, res) => {
   });
 };
 
+const createOpportunities = (req, res) => {
+  const newRows = req.body.newRows;
+
+  for (let row of newRows) {
+    db.query('INSERT INTO opportunities SET ?', row, (err, rows) => {
+      if (!err) { res.sendStatus(201); }
+    });
+  }
+};
+
 module.exports = {
-  getAllOpportunities
+  getAllOpportunities,
+  createOpportunities
 };
