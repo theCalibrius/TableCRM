3bdb45268d9bc5c0f53f796c9fed0bd5c1925916
Brian Pierce <brian@wanderfund.com>
2017-11-09 15:21:26 -0500

    "update paths, rename directory, add presents to babelrc"

--- __test__/App.test.js
+++ __test__/App.test.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../src/components/App';
+import App from '../client/src/components/App';
 
 const app = shallow(<App />);
 
