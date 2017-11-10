4eefa0fc999b53580ca23945c1f38df527f849f6
masato <hi@masato.io>
2017-11-09 22:18:44 -0800

    date convert via get api

--- client/src/container/Leads.jsx
+++ client/src/container/Leads.jsx
@@ -21,79 +21,77 @@ class Leads extends React.Component {
     return (
       <div>
         <div id="table">
-          {!this.props.leads ? (
-            <p>loading...</p>
-          ) : (
-            <HotTable
-              root="hot"
-              ref="hot"
-              settings={{
-                licenseKey: '',
-                data: this.props.leads,
-                dataSchema: {
-                  id: null,
-                  ownerId: null,
-                  description: null,
-                  firstName: null,
-                  lastName: null,
-                  suffix: null,
-                  title: null,
-                  value: null,
-                  email: null,
-                  phoneNumber: null,
-                  createdDate: null
-                },
-                colHeaders: [
-                  'id',
-                  'ownerId',
-                  'description',
-                  'firstName',
-                  'lastName',
-                  'suffix',
-                  'title',
-                  'value',
-                  'email',
-                  'phoneNumber',
-                  'createdDate'
-                ],
-                columns: [
-                  { data: 'id' },
-                  { data: 'ownerId' },
-                  { data: 'description' },
-                  { data: 'firstName' },
-                  { data: 'lastName' },
-                  { data: 'suffix' },
-                  { data: 'title' },
-                  {
-                    data: 'value',
-                    type: 'numeric',
-                    format: '$0,0.00'
+          {!this.props.leads
+            ? <p>loading...</p>
+            : <HotTable
+                root="hot"
+                ref="hot"
+                settings={{
+                  licenseKey: '',
+                  data: this.props.leads,
+                  dataSchema: {
+                    id: null,
+                    ownerId: null,
+                    description: null,
+                    firstName: null,
+                    lastName: null,
+                    suffix: null,
+                    title: null,
+                    value: null,
+                    email: null,
+                    phoneNumber: null,
+                    createdDate: null
                   },
-                  { data: 'email' },
-                  { data: 'phoneNumber' },
-                  {
-                    data: 'createdDate',
-                    type: 'date',
-                    dateFormat: 'MM/DD/YYYY',
-                    correctFormat: true
+                  colHeaders: [
+                    'id',
+                    'ownerId',
+                    'description',
+                    'firstName',
+                    'lastName',
+                    'suffix',
+                    'title',
+                    'value',
+                    'email',
+                    'phoneNumber',
+                    'createdDate'
+                  ],
+                  columns: [
+                    { data: 'id' },
+                    { data: 'ownerId' },
+                    { data: 'description' },
+                    { data: 'firstName' },
+                    { data: 'lastName' },
+                    { data: 'suffix' },
+                    { data: 'title' },
+                    {
+                      data: 'value',
+                      type: 'numeric',
+                      format: '$0,0.00'
+                    },
+                    { data: 'email' },
+                    { data: 'phoneNumber' },
+                    {
+                      data: 'createdDate',
+                      type: 'date',
+                      dateFormat: 'MM/DD/YYYY',
+                      correctFormat: true
+                    }
+                  ],
+                  rowHeaders: true,
+                  stretchH: 'all',
+                  contextMenu: ['remove_row', 'copy', 'cut'],
+                  filters: true,
+                  dropdownMenu: ['filter_by_condition', 'filter_by_value', 'filter_action_bar'],
+                  columnSorting: true,
+                  minSpareRows: 1,
+                  afterChange: (change, source) => {
+                    this.props.dispatch(createAndUpdateLeads(change, source).bind(this));
+                  },
+                  beforeRemoveRow: (index, amount) => {
+                    this.props.dispatch(removeLeads(index, amount).bind(this));
                   }
-                ],
-                rowHeaders: true,
-                stretchH: 'all',
-                contextMenu: ['remove_row', 'copy', 'cut'],
-                filters: true,
-                dropdownMenu: ['filter_by_condition', 'filter_by_value', 'filter_action_bar'],
-                columnSorting: true,
-                minSpareRows: 1,
-                afterChange: (change, source) => {
-                  this.props.dispatch(createAndUpdateLeads(change, source).bind(this));
-                },
-                beforeRemoveRow: (index, amount) => {
-                  this.props.dispatch(removeLeads(index, amount).bind(this));
-                }
-              }}
-            />
-          )}
+                }}
+              />}
         </div>
       </div>
     );
