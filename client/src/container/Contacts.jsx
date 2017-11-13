86728bc742339a2dd1ca63aebfa2d66af635c37f
Brian Pierce <brian@wanderfund.com>
2017-11-12 22:19:19 -0500

    Make small changes to contacts

--- client/src/container/Contacts.jsx
+++ client/src/container/Contacts.jsx
@@ -9,7 +9,7 @@ import axios from 'axios';
 import HotTable from 'react-handsontable';
 import 'handsontable-pro/dist/handsontable.full.js';
 // import 'handsontable-pro/dist/handsontable.full.css';
-
+ 
 class Contacts extends React.Component {
   // start of class
   constructor(props) {
