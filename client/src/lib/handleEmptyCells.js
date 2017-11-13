815ab166d699bff6df2aa5c4fc67428ce4243357
Joey Li <joeyzaozaoli@gmail.com>
2017-11-12 17:39:44 -0800

    Edit helper function #handleEmptyCells to apply it to all data types

--- client/src/lib/handleEmptyCells.js
+++ client/src/lib/handleEmptyCells.js
@@ -1,20 +1,7 @@
 export function handleEmptyCells(changes, source) {
-  let numericCols = {
-    estimatedValue: true,
-    winProbability: true
-  };
-
-  let dateCols = {
-    expectedCloseDate: true
-  };
-
   if (changes && source !== 'loadData') {
     for (let change of changes) {
-      let field = change[1];
-      if (field in numericCols || field in dateCols) {
-        if (change[3] === '') { change[3] = 'null'; }
-      }
+      if (change[3] === '') { change[3] = 'null'; }
     }
   }
 };
-
