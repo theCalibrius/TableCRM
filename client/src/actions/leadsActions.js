0ee4f2433decdadc1b6ebab159d3c9fcf47fa4b5
masato <hi@masato.io>
2017-11-13 23:51:37 -0800

    remove validatecell

--- client/src/actions/leadsActions.js
+++ client/src/actions/leadsActions.js
@@ -1,14 +1,13 @@
 import axios from 'axios';
 import moment from 'moment';
-import { getNewAndUpdatedRows, getRemovedIds, validateCellData } from '../lib/helper';
+import { getNewAndUpdatedRows, getRemovedIds } from '../lib/helper';
 
 export function getAllLeads(dispatch) {
   axios
     .get('/api/leads')
     .then(response => {
       for (const row of response.data) {
-        if (row.createdAt)
-          row.createdAt = moment(new Date(row.createdAt)).format('MM/DD/YYYY');
+        if (row.createdAt) row.createdAt = moment(new Date(row.createdAt)).format('MM/DD/YYYY');
       }
       return response;
     })
@@ -25,34 +24,26 @@ export function getAllLeads(dispatch) {
 
 export function createAndUpdateLeads(changes, source) {
   return function(dispatch) {
-    const validateCellDataBound = validateCellData.bind(this);
-    validateCellDataBound(changes, validationResult => {
-      if(validationResult){
-        const postCallback = function(newRows) {
-          axios.post('/api/leads', { newRows }).then(() => {
-            dispatch(getAllLeads);
-          });
-        };
+    const postCallback = function(newRows) {
+      axios.post('/api/leads', { newRows }).then(() => {
+        dispatch(getAllLeads);
+      });
+    };
 
-        const putCallback = function(updatedRows) {
-          axios.put('/api/leads', { updatedRows }).then(() => {
-            dispatch(getAllLeads);
-          });
-        };
+    const putCallback = function(updatedRows) {
+      axios.put('/api/leads', { updatedRows }).then(() => {
+        dispatch(getAllLeads);
+      });
+    };
 
-        const getNewAndUpdatedRowsBound = getNewAndUpdatedRows.bind(this);
-        getNewAndUpdatedRowsBound(changes, source, postCallback, putCallback);
-      }
-    });
+    const getNewAndUpdatedRowsBound = getNewAndUpdatedRows.bind(this);
+    getNewAndUpdatedRowsBound(changes, source, postCallback, putCallback);
   };
 }
 
 export function deleteLeads(index, amount) {
   return function(dispatch) {
-    // [startRow, startCol, endRow, endCol]
-    // selected rows
     const selectedRows = this.refs.hot.hotInstance.getSelected();
-    // get deleted row ID(s)
     const getRemovedIdsBound = getRemovedIds.bind(this);
     const removedIds = getRemovedIdsBound(selectedRows);
     axios({
