fa59706f532c798e745b268b826a2f6ab53c1163
Brian Pierce <brian@wanderfund.com>
2017-11-18 11:38:38 -0500

    Update CRUD functions for Accounts

--- client/src/actions/accountsActions.js
+++ client/src/actions/accountsActions.js
@@ -10,24 +10,31 @@ below, the function getAccounts is an action creator.
 */
 
 import axios from 'axios';
+import moment from 'moment';
 
 import { getNewAndUpdatedRows, getRemovedIds } from '../lib/helper';
 
-export function getAccounts(e) {
-  return function(dispatch) {
-    axios
-      .get('/api/accounts')
-      .then(response => {
-        console.log(response);
-        dispatch({
-          type: 'GET_ALL_ACCOUNTS',
-          payload: response.data
-        });
-      })
-      .catch(err => {
-        console.error.bind(err);
+export function getAccounts(dispatch) {
+  console.log('dispatch: ', dispatch);
+  axios
+    .get('/api/accounts')
+    .then(response => {
+      for ( let row of response.data ) {
+        if (row.createdAt) { 
+          row.createdAt = moment(new Date(row.createdAt)).format('MM/DD/YYYY');
+        }
+      }
+      return response;
+    })
+    .then(response => {
+      dispatch({
+        type: 'GET_ALL_ACCOUNTS',
+        payload: response.data
       });
-  };
+    })
+    .catch(err => {
+      console.error.bind(err);
+    });
 }
 
 export function createAndUpdateAccounts(changes, source) {
@@ -46,5 +53,18 @@ export function createAndUpdateAccounts(changes, source) {
 
     let getNewAndUpdatedRowsBound = getNewAndUpdatedRows.bind(this);
     getNewAndUpdatedRowsBound(changes, source, postCallback, putCallback);
+  };
+}
+
+export function deleteAccounts(index, amount) {
+  return function(dispatch) {
+    const selectedRows = this.refs.hot.hotInstance.getSelected();
+    const getRemovedIdsBound = getRemovedIds.bind(this);
+    const removedIds = getRemovedIdsBound(selectedRows);
+    axios({
+      method: 'DELETE',
+      url: '/api/accounts',
+      data: {removedIds}
+    });
   };
 }
