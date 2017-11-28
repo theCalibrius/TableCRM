d6c812cb85d099c7918203117c4e736265953733
masato <hi@masato.io>
2017-11-28 09:04:00 -0800

    dashboard opp total group by status backend api works

--- server/db/dashboard.js
+++ server/db/dashboard.js
@@ -1,12 +1,14 @@
 const db = require('./config');
-const lib = require('../lib/helper');
-const moment = require('moment');
+// const lib = require('../lib/helper');
 
 const getTotalOppValuePerStatus = (req, res) => {
-  db.query('SELECT * from opportunities', (err, rows) => {
-    if (err) console.log(err);
-    res.json(rows);
-  });
+  db.query(
+    'SELECT status, SUM (estimatedValue) from opportunities group by status',
+    (err, rows) => {
+      if (err) console.log(err);
+      res.json(rows);
+    }
+  );
 };
 
 module.exports = {
