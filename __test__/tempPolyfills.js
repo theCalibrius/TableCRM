41e451fc6bca1dca040c1508cfbdbc7ff93e653d
Brian Pierce <brian@wanderfund.com>
2017-11-28 18:42:02 -0500

    "Remove testing files from accounts branch"

--- __test__/tempPolyfills.js
+++
@@ -1,7 +0,0 @@
-// creates the gloabl request animation frame function that React now depends on for testing
-
-const requestAnimationFrame = (global.requestAnimationFrame = callback => {
-  setTimeout(callback, 0);
-});
-
-export default requestAnimationFrame;
